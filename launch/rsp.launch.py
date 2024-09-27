4bbbe19c7545b5d243db1add6b9867f46a1f6e39
StrTrek <30856158+strtrek@users.noreply.github.com>
2024-09-27 13:27:05 +0800

    安装机械臂

--- launch/rsp.launch.py
+++ launch/rsp.launch.py
@@ -15,12 +15,12 @@ def generate_launch_description():
     # Check if we're told to use sim time
     use_sim_time = LaunchConfiguration('use_sim_time')
 
-    # Process the URDF file
+    # 加载 URDF 描述文件 ( Process the URDF file )
     pkg_path = os.path.join(get_package_share_directory('panda_sar'))
     xacro_file = os.path.join(pkg_path,'description','robot.urdf.xacro')
     robot_description_config = xacro.process_file(xacro_file)
     
-    # Create a robot_state_publisher node
+    # 创建 robot_state_publisher 节点 ( Create a robot_state_publisher node )
     params = {'robot_description': robot_description_config.toxml(), 'use_sim_time': use_sim_time}
     node_robot_state_publisher = Node(
         package='robot_state_publisher',
@@ -29,13 +29,23 @@ def generate_launch_description():
         parameters=[params]
     )
 
+    # 运行 RViz2 ( rviz2 )
+    rviz2_config_file = os.path.join(pkg_path, "config", "main.rviz")
+    rviz2_node = Node(
+        package='rviz2',
+        executable='rviz2',
+        name='rviz2',
+        output='screen',
+        arguments=["-d", rviz2_config_file],
+    )
 
     # Launch!
     return LaunchDescription([
         DeclareLaunchArgument(
             'use_sim_time',
             default_value='false',
-            description='Use sim time if true'),
-
-        node_robot_state_publisher
+            description='Use sim time if true'
+        ),
+        node_robot_state_publisher,
+        rviz2_node
     ])
